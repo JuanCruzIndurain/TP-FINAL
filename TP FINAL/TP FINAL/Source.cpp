ebd68fdcf2b480edbf019471558f3c814923f8a8
Juan Cruz Indurain <juancindurain@gmail.com>
2019-06-19 11:41:21 -0300

    Acomode varias cosas e indente bien el programa. Tambien termine de hacer bien todos los constructores, quedan los de receta que son un poco mas complicados que todos los otros. Compile el programa y todo anda bien, ni un error. Hay un problema cuando lo compilas que te dice NULL ptr, pasa en la parte de simulador.cpp cuando hace: Porter->Insumos->AgregarItem(Extra); No se cual es la razon pero supongo cuando tengamos todo bien se va a solucionar solo.

--- TP FINAL/TP FINAL/Source.cpp
+++ TP FINAL/TP FINAL/Source.cpp
@@ -6,7 +6,7 @@ int main(void)
 	cSimulador *Simulador = new cSimulador();
 	int Tipo;
 
-	cout << "---- Indique el numero del tipo de cerveza pedido ---- " << endl;
+	cout << "----- Indique el numero del tipo de cerveza pedido ----- " << endl;
 	cout << "\n1 - Rubia" << "\n2 - Porter" << "\n3 - Honey" << "\n4 - IPA" << "\n5 - Irish Red" << "\nSeleccion: ";
 	cin >> Tipo;
 
