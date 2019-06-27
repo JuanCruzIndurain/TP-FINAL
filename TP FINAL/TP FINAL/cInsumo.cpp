03e9d9fd3dfb8b668ab62418700645e7b41abc13
Facundo Mogica <facundomogica@yahoo.com.ar>
2019-06-27 20:04:06 -0300

    Operadores

--- TP FINAL/TP FINAL/cInsumo.cpp
+++ TP FINAL/TP FINAL/cInsumo.cpp
@@ -6,8 +6,9 @@ cInsumo::cInsumo() : Nombre("")
 	Costo = 0;
 }
 
-cInsumo::cInsumo(string nombre, float Cantidad, float Costo) : Nombre(nombre)
+cInsumo::cInsumo(string nombre, float Cantidad, float Costo) 
 {
+	Nombre = nombre;
 	this->Cantidad = Cantidad;
 	this-> Costo = Costo;
 }
@@ -15,4 +16,38 @@ cInsumo::cInsumo(string nombre, float Cantidad, float Costo) : Nombre(nombre)
 cInsumo::~cInsumo()
 {
 
+}
+
+cInsumo * cInsumo::operator=(cInsumo & p)
+{
+	
+		p.Nombre = Nombre;
+		p.Cantidad = Cantidad;
+		p.Costo = Costo;
+	
+}
+
+bool cInsumo::operator==(cInsumo & p)
+{
+	if (p.Nombre == Nombre)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool cInsumo::operator!=(cInsumo & p)
+{
+
+	if (p.Nombre != Nombre)
+	{
+		return true;
+	}
+	return false;
+}
+
+ostream & operator<<(ostream & o, const cInsumo & i)
+{
+	o << "Nombre Insumo: " << i.Nombre << endl << "Cantidad: " << i.Cantidad << endl;
+	return o;
 }
--- TP FINAL/TP FINAL/cInsumo.h
+++ TP FINAL/TP FINAL/cInsumo.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ class cInsumo
 protected:
 	float Cantidad;
 	float Costo;
-	const string Nombre;
+	string Nombre;
 
 public:
 	cInsumo();
@@ -19,4 +20,28 @@ public:
 	float getCosto() const { return Costo; }
 	string getNombre() const { return Nombre; }
 	virtual int getTiempoHervor()const { return 0; }
+	friend ostream& operator<<(ostream &o, const cInsumo &i);
+	void setNombre(string nombre) { Nombre = nombre; }
+	void setCosto(float costo) { Costo = costo; }
+	void setCantidad(float cantidad) {Cantidad = cantidad; }
+	cInsumo* operator=(cInsumo &p);
+	bool operator==(cInsumo &p);
+	bool operator !=(cInsumo &p);
+
+	
 };
+istream& operator>>(istream &i, cInsumo &P)
+{
+	string aux;
+	float aux1;
+	cout << "Ingrese nombre insumo: " << endl;
+	i >> aux;
+	P.setNombre(aux);
+	cout << "Ingrese costo insumo: " << endl;
+	i >> aux1;
+	P.setCosto(aux1);
+	cout << "Ingrese cantidad insumo: " << endl;
+	i >> aux1;
+	P.setCantidad(aux1);
+	return i;
+}
